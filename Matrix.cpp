375a183e4641ae8bcab8a4a96845837de6af1c38
Daniil <stankevich.d@live.ac.cy>
2026-05-24 18:35:24 +0300

    Fix isIdentityMatrix non-diagonal check

--- Matrix.cpp
+++ Matrix.cpp
@@ -185,7 +185,7 @@ bool isIdentityMatrix(const double mat[][MAX_COL], const int maxRow) {
         for (int j = 0; j < maxRow; ++j) {
             if (i == j && mat[i][j] != 1)
                 return false;
-            else if (i != j || mat[i][j] != 0)
+            else if (i != j && mat[i][j] != 0)
                 return false;
         }
     }
