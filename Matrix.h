2ea3be0c2d32e1ce066a7fab9e3f11689b72e702
Daniil <stankevich.d@live.ac.cy>
2026-05-24 19:42:45 +0300

    Both old and advanced functions verisons.

--- Matrix.h
+++ Matrix.h
@@ -1,8 +1,8 @@
 /** \file Matrix.h
- * \brief Declarations for matrix utility functions.
- * \details This file contains constants and function prototypes for matrix operations.
+ * \brief Declaration of the Matrix class.
+ * \details This file contains the declaration of the Matrix class, which provides basic operations for matrix manipulation, including addition, multiplication, and transposition.
  * \author Daniil Stankevich
- * \date 2026-05-24
+ * \date 2026-05-20
  * \copyright University of Nicosia
  */
 
@@ -15,8 +15,8 @@ const int MAX_COL = 3;
 void printMatrix(const double mat[][MAX_COL], const int maxRow);
 void fillWithRandomNum(double mat[][MAX_COL], const int maxRow);
 
-double* sumOfRows(const double mat[][MAX_COL], const int maxRow);
-double* sumOfCols(const double mat[][MAX_COL], const int maxRow);
+double sumOfRow(const double mat[][MAX_COL], const int row, const int maxRow);
+double sumOfCol(const double mat[][MAX_COL], const int column, const int maxRow);
 
 double maxInMatrix(const double mat[][MAX_COL], const int maxRow);
 double minInMatrix(const double mat[][MAX_COL], const int maxRow);
@@ -25,5 +25,8 @@ double averageOfMatrix(const double mat[][MAX_COL], const int maxRow);
 bool isIdentityMatrix(const double mat[][MAX_COL], const int maxRow);
 bool makeIdentityMatrix(double mat[][MAX_COL], const int maxRow);
 double sumOfDiagonal(const double mat[][MAX_COL], const int maxRow);
+
+double* sumOfRows(const double mat[][MAX_COL], const int maxRow);
+double* sumOfCols(const double mat[][MAX_COL], const int maxRow);
 
 #endif
