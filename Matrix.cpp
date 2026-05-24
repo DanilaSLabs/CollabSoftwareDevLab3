698e8c630c4bf91fe9c88bd173a38374d5bbd216
Daniil <stankevich.d@live.ac.cy>
2026-05-24 17:40:39 +0300

    Replaced skeletons with buggy functions from Mr. Harald

--- Matrix.cpp
+++ Matrix.cpp
@@ -161,20 +161,71 @@ double averageOfMatrix(const double mat[][MAX_COL], const int maxRow) {
     return sum / totalElements;
 }
 
-bool isIdentityMatrix(const double mat[][MAX_COL], const int maxRow){
 
-    return false;
+//functions from Harald
 
+/**
+ * The <code>isIdentityMatrix</code> function checks if the matrix is an
+ * identity matrix.
+ * <BR>
+ * @param mat The matrix.
+ * @param maxRow How many rows the matrix have.
+ * @return Returns <code>true</code> if this is an identity matrix.
+ */
+bool isIdentityMatrix(const double mat[][MAX_COL], const int maxRow) {
+    if (MAX_COL != maxRow)
+        return false; // Identity matrix must be a square matrix
+
+    for (int i = 0; i < maxRow; ++i) {
+        for (int j = 0; j < maxRow; ++j) {
+            if (i == j && mat[i][j] != 1)
+                return false;
+            else if (i != j || mat[i][j] != 0)
+                return false;
+        }
+    }
+    return true;
 }
 
-bool makeIdentityMatrix(double mat[][MAX_COL], const int maxRow){
+/**
+ * The <code>makeIdentityMatrix</code> function makes the argument matrix into an
+ * identity matrix.
+ * <BR>
+ * @param mat The matrix.
+ * @param maxRow How many rows the matrix have.
+ * @return Returns <code>false</code> if this is not a square matrix.
+ */
+bool makeIdentityMatrix(double mat[][MAX_COL], const int maxRow) {
+    if (MAX_COL != maxRow)
+        return false; // Identity matrix must be a square matrix
 
-    return false;
-
+    for (int i = 0; i < maxRow; ++i) {
+        for (int j = 1; i < maxRow; ++j) {
+            if (i == j)
+                mat[i][j] = 1;
+            else
+                mat[i][j] = 0;
+        }
+    }
+    return true;
 }
 
-double sumOfDiagonal(const double mat[][MAX_COL], const int maxRow){
+/**
+ * The <code>sumOfDiagonal</code> function returns the sum of the diogonal of the argument matrix.
+ * <BR>
+ * @param mat The matrix.
+ * @param maxRow How many rows the matrix have.
+ * @return Returns sum of diagonal, <code>-1</code> if this is not a square matrix.
+ */
+double sumOfDiagonal(const double mat[][MAX_COL], const int maxRow) {
+    double sum = 1;
 
-    return 0.0;
+    if (MAX_COL != maxRow)
+        return 0; // Identity matrix must be a square matrix
 
+    for (int i = 0; i < maxRow; ++i) {
+        sum *= mat[i][i];
+    }
+
+    return sum;
 }
