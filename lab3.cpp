4bc9eaf3c4183738a708d0dd3ba48d0f911cbc61
Daniil <stankevich.d@live.ac.cy>
2026-05-24 19:48:08 +0300

    Made "advanced" mark for advanced functions in main menu

--- lab3.cpp
+++ lab3.cpp
@@ -115,7 +115,7 @@ int main() {
 
             case 11: {
                     double* rowSums = sumOfRows(mat, MAX_ROW);
-                    std::cout << "Sums of rows:" << std::endl;
+                    std::cout << "Sums of rows (advanced):" << std::endl;
                     for(int i = 0; i < MAX_ROW; ++i) {
                         std::cout << "Row " << i + 1 << ": " << rowSums[i] << std::endl;
                     }
@@ -125,7 +125,7 @@ int main() {
 
             case 12: {
                     double* colSums = sumOfCols(mat, MAX_ROW);
-                    std::cout << "Sums of columns:" << std::endl;
+                    std::cout << "Sums of columns (advanced):" << std::endl;
                     for(int i = 0; i < MAX_COL; ++i) {
                         std::cout << "Column " << i + 1 << ": " << colSums[i] << std::endl;
                     }
